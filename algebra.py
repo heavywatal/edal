e148bbc93d771a96d18bf9cbe5bd148f5195f09e
Watal M. Iwasaki <heavy.watal@gmail.com>
2014-09-25 03:27:48 +0900

    tweaked sympy to normalize Xi

--- algebra.py
+++ algebra.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+http://integrals.wolfram.com/index.jsp
 """
 from sympy import *
 
@@ -8,17 +9,24 @@ u, v = symbols('u v', real=True, positive=True)
 y0, y1 = symbols('y0 y1', real=True, positive=True)
 h0, h1 = symbols('h0 h1', real=True, positive=True)
 a = Symbol('a', real=True, positive=True)
-f = 2 / (1 - u) * (1 - (v / (1 - u)))
-g = (u * (1 - u)) ** (a - 1)
-h = 1 - h0 * ((u - y0) ** 2) - h1 * ((v - y1) ** 2)
-F = f * g * h
-print(F.simplify())
+
+triangle = 2 / (1 - u) * (1 - (v / (1 - u)))
+beta = (u * (1 - u)) ** (a - 1)
+F = beta * triangle
+
+Xi_approx = 1 - h0 * ((u - y0) ** 2) - h1 * ((v - y1) ** 2)
+Xi_normal = exp( - h0 * ((u - y0) ** 2) - h1 * ((v - y1) ** 2))
+T = F * Xi_approx
+
 
 with assuming(Q.positive(a - 1)):
-    G = integrate(F, (v, 0, 1 - u))
-    print(G.simplify())
+    print('D_I')
+    D_I = integrate(T, (v, 0, 1 - u))
+    print(D_I.simplify())
 
-#    H = integrate(G, (u, 0, 1))
-#    print(H)
+    print('Xi_approx')
+    print(integrate(Xi_approx, (v, 0, 1 - u)).simplify())
+    #(u - 1)*(3*h0*u**2 - 6*h0*u*y0 + 3*h0*y0**2 + 3*h1*y1**2 + 3*h1*y1*(u - 1) + h1*(u - 1)**2 - 3)/3
 
-#http://integrals.wolfram.com/index.jsp
+    print('Xi_normal')
+    print(integrate(Xi_normal, (v, 0, 1 - u)).simplify())
